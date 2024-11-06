51c6ee44db26286e45d83a81c76b51a97cc7403f
AIM-NSorachi <a8123160@ima.aim.aoyama.ac.jp>
2024-11-06 15:49:06 +0900

    fix:ページネーションボタンのホバースタイル

--- src/app/components/pagination.tsx
+++ src/app/components/pagination.tsx
@@ -16,22 +16,22 @@ export const Pagination = ({
     <ul className="flex items-center justify-center space-x-4 mt-8">
       <li>
         {currentPage === 1 ? (
-          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg">
+          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg hover:opacity-70">
             <span>最初へ</span>
           </div>
         ) : (
-          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg">
+          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg hover:opacity-70">
             <Link href={`/${basePath}/1`}>最初へ</Link>
           </div>
         )}
       </li>
       <li>
         {currentPage === 1 ? (
-          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg">
+          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg hover:opacity-70">
             <span>前へ</span>
           </div>
         ) : (
-          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg">
+          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg hover:opacity-70">
             <Link href={`/${basePath}/${currentPage - 1}`}>前へ</Link>
           </div>
         )}
@@ -43,22 +43,22 @@ export const Pagination = ({
       </li>
       <li>
         {currentPage === totalPages ? (
-          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg">
+          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg hover:opacity-70">
             <span>次へ</span>
           </div>
         ) : (
-          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg">
+          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg hover:opacity-70">
             <Link href={`/${basePath}/${currentPage + 1}`}>次へ</Link>
           </div>
         )}
       </li>
       <li>
         {currentPage === totalPages ? (
-          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg">
+          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg hover:opacity-70">
             <span>最後へ</span>
           </div>
         ) : (
-          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg">
+          <div className="p-2 px-3 bg-[#F6F3EA] rounded-md shadow text-lg hover:opacity-70">
             <Link href={`/${basePath}/${totalPages}`}>最後へ</Link>
           </div>
         )}
