9657401a4b0c841c7e856c1051a18fb879366578
櫻井　香乃佳 <a5822057@aoyama.jp>
2025-01-07 12:11:29 +0900

    fix: 画像サイズの修正

--- src/app/components/rule.tsx
+++ src/app/components/rule.tsx
@@ -6,10 +6,10 @@ type Props = {
 
 const Rule = ({ image, text, note }: Props) => {
     return (
-        <div className="mb-4 rounded-lg bg-white p-6">
-            <img src={image} alt="" />
-            <p>{text}</p>
-            <p className="text-[#8C8C8C]">{note}</p>
+        <div className="mb-4 flex flex-col items-center rounded-lg bg-white p-2 sm:p-6">
+            <img  className="w-4/5"src={image} alt="" />
+            <p className="leading-5">{text}</p>
+            <p className="mt-3 text-[#8C8C8C] leading-5">{note}</p>
         </div>
     );
 };
