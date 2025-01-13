578fb8c6c200a8122dd4e5cb7536d9c30e64c842
AIM-OKohei <ab122052@ima.aim.aoyama.ac.jp>
2025-01-14 06:59:31 +0900

    fix: 業務紹介ページの文章差し替え

--- src/app/components/jobIntroduction.tsx
+++ src/app/components/jobIntroduction.tsx
@@ -2,19 +2,18 @@ import Image from "next/image";
 
 type Props = {
 	image: string;
-	alt: string;
 	title: string;
 	text: React.ReactNode;
 };
 
-const JobIntroduction = ({ image, alt, title, text }: Props) => {
+const JobIntroduction = ({ image, title, text }: Props) => {
 	return (
 		<div className="mx-[8%] my-[8%] flex flex-col rounded-md bg-white p-4">
 			{/* 画像の表示 */}
 			<div className="mb-4 w-full">
 				<Image
 					src={image}
-					alt={alt}
+					alt={title}
 					width={640}
 					height={360}
 					className="rounded-md"
@@ -23,7 +22,7 @@ const JobIntroduction = ({ image, alt, title, text }: Props) => {
 			{/* テキストエリア */}
 			<div className="text-left">
 				<div className="mb-2 font-bold text-xl">{title}</div>
-				<div className="text-sm">{text}</div>
+				<div className="text-base">{text}</div>
 			</div>
 		</div>
 	);
