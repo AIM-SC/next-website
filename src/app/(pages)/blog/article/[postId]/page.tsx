a534a86a290526e7102906d77c1fe53981d8a8c6
AIM-NSorachi <a8123160@ima.aim.aoyama.ac.jp>
2025-01-14 09:59:59 +0900

    fix:余計な閉じタグを削除

--- src/app/(pages)/blog/article/[postId]/page.tsx
+++ src/app/(pages)/blog/article/[postId]/page.tsx
@@ -29,7 +29,7 @@ export default async function StaticDetailPage({
 	return (
 		<div className="py-[75px] text-[20px] text-black leading-10">
 			<div className="mb-[3%] flex flex-col gap-4 px-[13%]">
-			<div className="text-center font-bold text-[#d9ae4c] text-sm md:text-base">
+				<div className="text-center font-bold text-[#d9ae4c] text-sm md:text-base">
 					BLOG
 				</div>
 				<div className="text-center font-semibold text-2xl text-black md:text-4xl">
