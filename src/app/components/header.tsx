fa617c68142655c9f89e0342b08a40783259b0ac
AIM-OKohei <ab122052@ima.aim.aoyama.ac.jp>
2025-01-29 20:56:58 +0900

    fix: 順番変更

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -13,9 +13,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
-
 	return (
-
 		<header className="h-[100px] border-b">
 			<div className="container mx-auto flex h-full items-center justify-between px-[15px]">
 				<Link href="/">
@@ -110,10 +108,10 @@ export default function Header() {
 									<a href={"/info"}>お知らせ</a>
 								</li>
 								<li className={cn("px-5 py-3")}>
-									<a href={"/blog"}>技術ブログ</a>
+									<a href={"/movies"}>YouTube動画</a>
 								</li>
 								<li className={cn("px-5 py-3")}>
-									<a href={"/movies"}>YouTube動画</a>
+									<a href={"/blog"}>ブログ</a>
 								</li>
 							</ul>
 							<SheetHeader className="mb-[10px] h-[20px] border-b" />
@@ -121,31 +119,13 @@ export default function Header() {
 								もっとAIM Commonsに関わりたい方へ
 							</p>
 							<ul>
-								<li className={cn("px-5 py-3")} >
+								<li className={cn("px-5 py-3")}>
 									<a href={"/recruit"}>学生スタッフ採用</a>
 								</li>
 							</ul>
 						</SheetContent>
 					</Sheet>
 				</div>
-
-				{/*lg以上でのメニュー*/}
-				{/* <ul className="hidden items-center lg:flex">
-                    {MenuList.map((item, i) => (
-                        <li key={item.value} className={
-                            cn(
-                                "border-r",
-                                "border-dashed",
-                                "px-5 py-3",
-                                "border-black",
-                                i === 0 && "border-l"
-                            )}>
-                            <Link href={`/${item.value}`}>
-                                {item.label}
-                            </Link>
-                        </li>
-                    ))}
-                </ul> */}
 			</div>
 		</header>
 	);
